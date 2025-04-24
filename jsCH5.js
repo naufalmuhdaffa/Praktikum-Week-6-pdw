4124378eec7b55b2fb89819923646c4b13b453f9
naufalmuhdaffa <naufalmuhdaffa@gmail.com>
2025-04-24 16:13:16 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+  alert("ini video Yakult");
+});
+
+audio.addEventListener("click", function audio() {
+  alert("ini Audio Negeri Abadi");
+});
+
+gambar.addEventListener("click", function gambar() {
+  alert("ini gambar Kemasan Sachet");
+});
